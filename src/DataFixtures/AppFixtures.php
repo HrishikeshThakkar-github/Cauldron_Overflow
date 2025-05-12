eb0210a70f775963cf2da3998691d31b07212b93
HrishikeshThakkar-github <hrishi.pvt@gmail.com>
2025-05-12 17:06:46 +0530

    learnt about how with help of foundry we can add dummy data more swiftly in comparison to fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Question;
+use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,16 +12,21 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $question = new Question();
-        $question->setName('name  of new question')
-            ->setSlug('name of new question'.rand(1,100))
-            ->setQuestion('qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq')
-            ->setAskedAt(new \DateTime('now'));
-        //dd($question);
+//        $question = new Question();
+//        $question->setName('name  of new question')
+//            ->setSlug('name of new question'.rand(1,100))
+//            ->setQuestion('qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
+//            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq')
+//            ->setAskedAt(new \DateTime('now'));
+//        //dd($question);
+//
+//        $question->setVotes(rand(0,50));
+        QuestionFactory::new()->createMany(20);
 
-        $question->setVotes(rand(0,50));
-        $manager->persist($question);
+        QuestionFactory::new()
+            ->unpublished()
+            ->createMany(5)
+        ;
 
         $manager->flush();
     }
